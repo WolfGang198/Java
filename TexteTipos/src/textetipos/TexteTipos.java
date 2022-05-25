47b7dfadca4d347f0784418a5ec5c40452caf5f8
WolfGang <95577533+WolfGang198@users.noreply.github.com>
2022-05-25 14:56:41 -0300

    Add files via upload
    apenas um texte... / just a text...

---
+++ TexteTipos/src/textetipos/TexteTipos.java
@@ -0,0 +1,15 @@
+
+package textetipos;
+
+public class TexteTipos {
+
+    public static void main(String[] args) {
+       /*int idade = 30
+        *String valor = Integer.toString(idade);
+        *System.out.println(valor);
+        */
+       String valor = "30";
+       int idade = Integer.parseInt(valor);
+    }
+    
+}
