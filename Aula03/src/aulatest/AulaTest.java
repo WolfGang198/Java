d3dad4cab2323c2b880bf449fd3a0d4fd2608ecc
WolfGang <95577533+WolfGang198@users.noreply.github.com>
2022-08-30 16:51:33 -0300

    Add files via upload

---
+++ Aula03/src/aulatest/AulaTest.java
@@ -0,0 +1,16 @@
+
+package aulatest;
+
+public class AulaTest {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.modelo = "BIC cristal";
+        c1.cor = "Azul";
+        c1.carga = 80;
+        c1.tampar();
+        c1.status();
+        c1.rabiscar();
+    }
+    
+}
---
+++ Aula03/src/aulatest/Caneta.java
@@ -0,0 +1,35 @@
+
+package aulatest;
+
+public class Caneta {
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
+    
+    public void status(){
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Esta tamapada? " + this.tampada);
+    }
+    
+    public void rabiscar(){
+        if(this.tampada == true){
+            System.out.println("ERROR !!!");
+        }
+        else{
+            System.out.println("Cogito Ergo Sul");
+        }
+    }
+    
+    public void tampar(){
+        this.tampada = true;
+    }
+    
+    public void destampar(){
+        this.tampada = false;
+    }
+}
