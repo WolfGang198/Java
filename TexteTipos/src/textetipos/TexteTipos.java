a06429bde85612b4d1a5c0af1eb7b585722d2dc4
WolfGang <95577533+WolfGang198@users.noreply.github.com>
2022-10-19 15:10:17 -0300

    Add files via upload

--- TexteTipos/src/textetipos/TexteTipos.java
+++ TexteTipos/src/textetipos/TexteTipos.java
@@ -1,15 +1,17 @@
-
 package textetipos;
 
 public class TexteTipos {
 
     public static void main(String[] args) {
-       /*int idade = 30
-        *String valor = Integer.toString(idade);
-        *System.out.println(valor);
-        */
-       String valor = "30";
-       int idade = Integer.parseInt(valor);
+      /* 
+       *int idade = 30;
+       *String valor = Integer.toString(idade);
+       *System.out.println(valor);
+       */
+      
+        String valor = "30.5";
+        float idade = Float.parseFloat(valor);
+        System.out.println(idade);
     }
     
 }
