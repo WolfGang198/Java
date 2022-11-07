fd6c7f515f48b9a5bdaf9fbc0dfbaf680e1d9c9d
WolfGang <95577533+WolfGang198@users.noreply.github.com>
2022-11-07 15:44:27 -0300

    Add files via upload

---
+++ OperadorTernario/src/operadorternario/OperadorTernario.java
@@ -0,0 +1,14 @@
+
+package operadorternario;
+
+public class OperadorTernario {
+
+    public static void main(String[] args) {
+        int n1, n2, r;
+        n1 = 4;
+        n2 = 8;
+        r = n1 > n2 ? n1+n2 : n2-n1;
+        System.out.println(r);
+    }
+    
+}
