1c8759e756a372c78af9158bdecfdc87731d16cb
WolfGang <95577533+WolfGang198@users.noreply.github.com>
2022-11-09 11:09:55 -0300

    Add files via upload

---
+++ ProgramaMedia/src/programamedia/ProgramaMedia.java
@@ -0,0 +1,20 @@
+package programamedia;
+
+import java.util.Scanner;
+
+public class ProgramaMedia {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Primeira nota: ");
+        float n1 = teclado.nextFloat();
+        System.out.print("Segunda nota: ");
+        float n2 = teclado.nextFloat();
+        float m = (n1 + n2)/2;
+        System.out.println("Sua media foi " + m);
+        if (m>9){
+            System.out.println("Parabens, pequeno gafanhoto!!!");
+        }
+    }
+    
+}
