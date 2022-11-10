cba714cb92c2b59e90d42867c8e9e1e068a9da59
WolfGang <95577533+WolfGang198@users.noreply.github.com>
2022-11-10 15:27:22 -0300

    Add files via upload

---
+++ Repeticao01/src/repeticao01/Repeticao01.java
@@ -0,0 +1,14 @@
+
+package repeticao01;
+
+public class Repeticao01 {
+
+    public static void main(String[] args) {
+        int cc = 0;
+        do{
+            System.out.println(cc);
+            cc++;
+        } while(cc < 4);
+    }
+    
+}
