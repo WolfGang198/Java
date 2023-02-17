43637d87370ab07e83e50d14ab9df736a06eb897
WolfGang <95577533+WolfGang198@users.noreply.github.com>
2023-02-17 15:33:02 -0300

    Add files via upload

--- Aula04/src/aula04/Aula04.java
+++ Aula04/src/aula04/Aula04.java
@@ -2,10 +2,13 @@
 package aula04;
 
 public class Aula04 {
-
+    
     public static void main(String[] args) {
-       Caneta c1 = new Caneta("BIC", "Amarela", 0.7f);
-       c1.status();
+        Caneta c1 = new Caneta("AJAX", "Preta", 0.6f);
+        c1.Status();
+        
+        Caneta c2 = new Caneta("AJAX", "Vermelha", 1.3f);
+        c2.Status();
     }
     
 }
--- Aula04/src/aula04/Caneta.java
+++ Aula04/src/aula04/Caneta.java
@@ -1,16 +1,48 @@
+
 package aula04;
 
 public class Caneta {
     public String modelo;
-    private float ponta;
+    private Float ponta;
     private String cor;
     private boolean tampada;
     
-    public Caneta(String m, String c, float p){
-        this.cor = c;
+    public Caneta(String m, String c, Float p){//metodo construtor
         this.modelo = m;
+        this.cor = c;
         this.ponta = p;
         this.tampar();
     }
     
+    public String getModelo(){
+        return this.modelo;
+    }
+    
+    public void setModelos(String m){
+        this.modelo = m;
+    }
+    
+    public Float getPonta(){
+        return this.ponta;
+    }
+    
+    public void setPonta(Float p){
+        this.ponta = p;
+    }
+    
+    public void tampar(){
+        this.tampada = true;
+    }
+    
+    public void destampar(){
+        this.tampada = false;
+    }
+    
+    public void Status(){
+        System.out.println("SOBRE A CANETA:");
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Ponta: " + this.getPonta());
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Tamapada: " + this.tampada);
+    }
 }
