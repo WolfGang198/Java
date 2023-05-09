f020bbcf7efa064e45048e7afbbe2f16e60736d6
WolfGang <95577533+WolfGang198@users.noreply.github.com>
2023-05-09 14:55:41 -0300

    Add files via upload

--- arrayListTest/src/arraylisttest/ArrayListTest.java
+++ arrayListTest/src/arraylisttest/ArrayListTest.java
@@ -1,17 +1,26 @@
 
 package arraylisttest;
-import java.util.ArrayList;
+
+import java.util.ArrayList;//importação
 
 public class ArrayListTest {
 
     public static void main(String[] args) {
-        ArrayList<String> minhaLista = new ArrayList<>();
-        minhaLista.add(1, "A");
-        minhaLista.add(2, "B");
-        minhaLista.add(3, "C"); //adicionar
+        ArrayList<String> contatos = new ArrayList<>();//chamado
+        contatos.add("Rafael");
+        contatos.add("99400-9087");
+        contatos.add("rafael_marques@furro.com");
+        contatos.add("Jonas");
+        contatos.add("99393-3547");
+        contatos.add("jojorge@pirado.com");//adicionando elementos...
         
-        String elemento = minhaLista.get(2); //acessar
-        System.out.println(elemento);
+        System.out.println("Tamanho do Array: " + contatos.size());//verificando tamanho
+        System.out.println(contatos);//mostrar tudo
+        System.out.println("Email Rafael: " + contatos.get(2));//mostrar especifico
+        System.out.println("===================");
+        for(int i = 0; i < contatos.size(); i++){
+            System.out.println(contatos.get(i));//percorrer array
+        }
     }
     
 }
